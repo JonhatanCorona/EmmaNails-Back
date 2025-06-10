20b78d0ec37502c21bacd85e8e4ea7427461f374
Jonhatan Corona <coronajonhatan@gmail.com>
2025-06-10 17:06:22 -0500

    se modifica cors

--- back/src/server.ts
+++ back/src/server.ts
@@ -8,7 +8,12 @@ const server = express();
 
 server.use(express.json());
 server.use(morgan("dev"));
-server.use(cors());
+server.use(cors({
+  origin: ['http://localhost:5173', 'https://emmanails-back-production.up.railway.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 server.use(router)
 
